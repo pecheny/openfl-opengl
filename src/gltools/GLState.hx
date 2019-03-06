34e58b8fa42708ac5d0332f8a182c20d71c4c083
artem pecheny <pecheny@gmail.com>
2019-03-06 03:19:23 +0300

    Initial WebGL compatibility.

--- src/gltools/GLState.hx
+++ src/gltools/GLState.hx
@@ -11,6 +11,8 @@ import lime.utils.DataView;
 import lime.graphics.WebGLRenderContext;
 #end
 
+//using lime.utils.ArrayBufferView.ArrayBufferIO;
+
 /**
 *  Incapsulates work with different attributes on single buffer.
 *  Calculates and stores offset, provides hi-level api to add/set vertex data by attribute and index;
@@ -23,8 +25,8 @@ class GlState {
     public var buffer:GLBuffer;
     public var program:GLProgram;
     var stride:Int = 0;
-    var data:DataView; // обертка над эррэйбуфер = абстракт над хакси.йо.байтез
-    var bufferData:ArrayBufferView;
+    var data:DataContainer;
+//    var arrayBuffer:ArrayBuffer;
 
     public function new(gl:WebGLRenderContext, program:GLProgram) {
         this.gl = gl;
@@ -46,10 +48,11 @@ class GlState {
         gl.bindBuffer(gl.ARRAY_BUFFER, null);
     }
 
-    public function initDataContainer(vertCount){
+    public function initDataContainer(vertCount) {
         var size = stride * vertCount;
-        var buffer = new ArrayBuffer(size);
-        data = new DataView(buffer);
+        var arrayBuffer = new ArrayBuffer(size);
+        data = DataContainer.fromBuffer(arrayBuffer);
+//        var buffer = new ArrayBuffer(size);
     }
 
     public function enambleAttributes() {
@@ -64,13 +67,12 @@ class GlState {
         }
     }
 
-    @:access(lime.utils.ArrayBufferView)
     public function initData() {
-        bufferData = new ArrayBufferView(null, None);
-        bufferData.initBuffer(data.buffer);
+//        bufferData = new ArrayBufferView(null, None);
+//        bufferData.initBuffer(data.buffer);
         gl.useProgram(program);
         gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
-        gl.bufferData(gl.ARRAY_BUFFER, bufferData, gl.STATIC_DRAW);
+        gl.bufferData(gl.ARRAY_BUFFER, data, gl.STATIC_DRAW);
     }
 
     @:access(lime.utils.ArrayBufferView)
@@ -98,6 +100,46 @@ class GlState {
             case float32 : data.setFloat32(offset, value);
         }
     }
-
-
 }
+@:forward(setInt8, setFloat32, setInt16, setInt32, setUint8, setUint16, setUint32)
+abstract JsDataContainer(DataView) to DataView {
+    inline function new(val) this = val;
+
+    public static inline function fromBuffer(buffer:ArrayBuffer) return new JsDataContainer(new DataView(buffer));
+}
+
+#if !js
+abstract NativeDataContainer(ArrayBufferView) to ArrayBufferView {
+    inline function new(val) this = val;
+
+    public function setInt8(byteOffset:Int, value:Int)
+    ArrayBufferIO.setInt8(this.buffer, byteOffset, value);
+
+    public function setInt16(byteOffset:Int, value:Int)
+    ArrayBufferIO.setInt16(this.buffer, byteOffset, value);
+
+    public function setInt32(byteOffset:Int, value:Int)
+    ArrayBufferIO.setInt32(this.buffer, byteOffset, value);
+
+    public function setUint8(byteOffset:Int, value:Int)
+    ArrayBufferIO.setUint8(this.buffer, byteOffset, value);
+
+    public function setUint16(byteOffset:Int, value:Int)
+    ArrayBufferIO.setUint16(this.buffer, byteOffset, value);
+
+    public function setUint32(byteOffset:Int, value:Int)
+    ArrayBufferIO.setUint32(this.buffer, byteOffset, value);
+
+    public function setFloat32(byteOffset:Int, value:Float)
+    ArrayBufferIO.setFloat32(this.buffer, byteOffset, value);
+
+    @:access(lime.utils.ArrayBufferView)
+    public static inline function fromBuffer(buffer:ArrayBuffer) {
+        var buf = new ArrayBufferView(null, None);
+        buf.initBuffer(buffer);
+        return new NativeDataContainer(buf);
+    }
+}
+#end
+
+typedef DataContainer = #if js JsDataContainer #else NativeDataContainer #end
