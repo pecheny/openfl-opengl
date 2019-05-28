47348a2cbcefd5e593a53a807cf66fca20a77337
artem pecheny <pecheny@gmail.com>
2019-05-29 00:42:49 +0300

    Extract wrapper for bytes.

---
+++ src/gltools/ByteDataWriter.hx
@@ -0,0 +1,35 @@
+package gltools;
+import haxe.io.Bytes;
+import lime.utils.ArrayBufferView.ArrayBufferIO;
+abstract ByteDataWriter(Bytes) to Bytes from Bytes {
+    inline function new(val) this = val;
+
+    public inline function setInt8(byteOffset:Int, value:Int)
+    ArrayBufferIO.setInt8(this, byteOffset, value);
+
+    public inline function setInt16(byteOffset:Int, value:Int)
+    ArrayBufferIO.setInt16(this, byteOffset, value);
+
+    public inline function setInt32(byteOffset:Int, value:Int)
+    ArrayBufferIO.setInt32(this, byteOffset, value);
+
+    public inline function setUint8(byteOffset:Int, value:Int)
+    ArrayBufferIO.setUint8(this, byteOffset, value);
+
+    public inline function setUint16(byteOffset:Int, value:Int)
+    ArrayBufferIO.setUint16(this, byteOffset, value);
+
+    public inline function setUint32(byteOffset:Int, value:Int)
+    ArrayBufferIO.setUint32(this, byteOffset, value);
+
+    public inline function setFloat32(byteOffset:Int, value:Float)
+    ArrayBufferIO.setFloat32(this, byteOffset, value);
+
+//    @:access(lime.utils.ArrayBufferView)
+//    public static inline function fromBuffer(buffer:ArrayBuffer) {
+//        var buf = new ArrayBufferView(null, None);
+//        buf.initBuffer(buffer);
+//        return new NativeDataContainer(buf);
+//    }
+}
+
--- src/gltools/VertexBuilder.hx
+++ src/gltools/VertexBuilder.hx
@@ -1,6 +1,5 @@
 package gltools;
 import haxe.io.Bytes;
-import lime.utils.ArrayBufferView.ArrayBufferIO;
 class VertexBuilder {
     var data:ByteDataWriter;
     var a:AttribSet;
@@ -51,35 +50,4 @@ class VertexBuilder {
 
 interface ValueProvider<T> {
     function getValue(vertIdx:Int):T;
-}
-abstract ByteDataWriter(Bytes) to Bytes from Bytes {
-    inline function new(val) this = val;
-
-    public inline function setInt8(byteOffset:Int, value:Int)
-    ArrayBufferIO.setInt8(this, byteOffset, value);
-
-    public inline function setInt16(byteOffset:Int, value:Int)
-    ArrayBufferIO.setInt16(this, byteOffset, value);
-
-    public inline function setInt32(byteOffset:Int, value:Int)
-    ArrayBufferIO.setInt32(this, byteOffset, value);
-
-    public inline function setUint8(byteOffset:Int, value:Int)
-    ArrayBufferIO.setUint8(this, byteOffset, value);
-
-    public inline function setUint16(byteOffset:Int, value:Int)
-    ArrayBufferIO.setUint16(this, byteOffset, value);
-
-    public inline function setUint32(byteOffset:Int, value:Int)
-    ArrayBufferIO.setUint32(this, byteOffset, value);
-
-    public inline function setFloat32(byteOffset:Int, value:Float)
-    ArrayBufferIO.setFloat32(this, byteOffset, value);
-
-//    @:access(lime.utils.ArrayBufferView)
-//    public static inline function fromBuffer(buffer:ArrayBuffer) {
-//        var buf = new ArrayBufferView(null, None);
-//        buf.initBuffer(buffer);
-//        return new NativeDataContainer(buf);
-//    }
 }
