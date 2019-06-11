a0cf94bc741e38270f84ea72afef47d70ffd013e
artem pecheny <pecheny@gmail.com>
2019-06-11 20:53:33 +0300

    Mesh asset loader draft.

--- src/Main.hx
+++ src/Main.hx
@@ -1,27 +1,55 @@
 package ;
-import oglrenderer.OGLContainerCopy;
+import mesh.AssetMeshProvider;
+import shader.DummyShader;
+import openfl.display.OpenGLRenderer;
+import gltools.sets.ColorSet;
+import mesh.StaticMeshProvider;
+import oglrenderer.GLLayer;
 import oglrenderer.OGLContainer;
+import oglrenderer.OGLContainerCopy;
 import openfl.display.Sprite;
+import openfl.events.RenderEvent;
 #if !boo
 import lime.graphics.WebGLRenderContext;
 #end
-class Main extends Sprite{
+class Main extends Sprite {
+    var c:GLLayer<ColorSet>;
+    var inited = false;
+
     public function new() {
         super();
+        addEventListener(RenderEvent.RENDER_OPENGL, render);
         var a = new OGLContainer();
         var b = new OGLContainerCopy();
-        addChild(a);
-        addChild(new Pointer());
-        addChild(b);
+        c = new GLLayer<ColorSet>();
+        c.addView(new AssetMeshProvider<ColorSet>("Assets/my_tri", ColorSet.instance));
+//        c.addView(new StaticMeshProvider<ColorSet>());
+
+//        addChild(a);
+//        addChild(new Pointer());
+//        addChild(b);
+//        addChild(c);
     }
+
+    function render(e) {
+        if (inited)
+            return;
+        trace("init");
+        inited = true;
+        var renderer:OpenGLRenderer = cast e.renderer;
+        var gl:WebGLRenderContext = renderer.gl;
+        c.init(gl, DummyShader.createDummyShader(gl), ColorSet.instance);
+        addChild(c);
+    }
+
 
 }
 
 class Pointer extends Sprite {
-    public function new () {
+    public function new() {
         super();
         graphics.beginFill(0xff0040);
-        graphics.drawCircle(100,100,100);
+        graphics.drawCircle(100, 100, 100);
         graphics.endFill();
     }
 }
---
+++ src/mesh/AssetMeshProvider.hx
@@ -0,0 +1,48 @@
+package mesh;
+import gltools.VertDataProvider;
+import lime.utils.UInt16Array;
+import gltools.AttribSet;
+import haxe.io.Bytes;
+class AssetMeshProvider <T:AttribSet> implements VertDataProvider<T>{
+    var data:Bytes;
+    var inds:Bytes;
+    var stride:Int;
+
+    public function AssetMeshProvider(path:String, attrs:T) {
+        data = lime.utils.Assets.getBytes(path + "/bytes");//sys.io.File.getBytes(path + "bytes")
+        inds = lime.utils.Assets.getBytes(path + "/inds");//sys.io.File.getBytes(path + "bytes")
+        stride = attrs.stride;
+
+//        var builder = new VertexBuilder(ColorSet.instance);
+//        builder.setTarget(Bytes.alloc(3 * ColorSet.instance.stride));
+//        var pos = new TriPosProvider(1, -1);
+//        var color = new SolidColorProvider(128, 10, 100);
+//        builder.regSetter(AttribAliases.NAME_POSITION, pos.getPos);
+//        builder.regSetter(AttribAliases.NAME_CLOLOR_IN, color.getCC);
+//        builder.fetchFertices(3);
+//        data = builder.getData();
+//        inds = Bytes.alloc(3 * UInt16Array.BYTES_PER_ELEMENT);
+//        for (i in 0...3)
+//            inds.setUint16(i * UInt16Array.BYTES_PER_ELEMENT, i);
+    }
+
+    public function getVerts():Bytes {
+        return data;
+    }
+
+    public function getInds() {
+        return inds;
+    }
+
+    public function getVertsCount():Int {
+        var stride1 = Std.int(data.length / stride);
+        trace("Cert cnt:" + stride1);
+        return stride1;
+    }
+
+    public function getIndsCount():Int {
+        var ic = Std.int(inds.length / UInt16Array.BYTES_PER_ELEMENT);
+        trace("Ind cnt: " + ic);
+        return ic;
+    }
+}
