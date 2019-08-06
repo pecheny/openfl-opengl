001e4e3e8088002c654fa6ffe98abcaeac7eb0f6
artem pecheny <pecheny@gmail.com>
2019-08-06 17:51:27 +0300

    ByteKeyrame.toString().

--- src/animation/keyframes/ByteKeyframe.hx
+++ src/animation/keyframes/ByteKeyframe.hx
@@ -1,20 +1,34 @@
 package animation.keyframes;
-import data.DataType;
 import data.BytesView;
+import data.DataType;
 import haxe.io.Bytes;
 class ByteKeyframe extends BytesView implements Keyframe {
     var time:Float;
-    var vertCount:Int
+    var vertCount:Int;
+
     public function new(time, bytes:Bytes, offset:Int, stride:Int, numComponent:Int, componentType:DataType, vertCount:Int) {
         this.time = time;
         this.vertCount = vertCount;
-        super(bytes, offset,stride, numComponent, componentType);
+        super(bytes, offset, stride, numComponent, componentType, 0);
     }
-    public function getTime():Float{
+
+    public function getTime():Float {
         return time;
     }
 
     public function getVertCount():Int {
         return vertCount;
+    }
+
+    public function toString() {
+        var r = "";
+        for (i in 0...vertCount) {
+            r += "[ ";
+            for (c in 0...numComponent) {
+                r+= getValue(i, c) + ", ";
+            }
+            r += "], ";
+        }
+        return r;
     }
 }
