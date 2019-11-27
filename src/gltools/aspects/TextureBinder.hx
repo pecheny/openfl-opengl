317e0c14d05700c89aaf32b934cf25320375300e
artem pecheny <pecheny@gmail.com>
2019-11-27 03:03:26 +0300

    Texture binder.

---
+++ src/gltools/aspects/TextureBinder.hx
@@ -0,0 +1,43 @@
+package gltools.aspects;
+import lime.graphics.Image;
+import data.aliases.UniformAliases;
+import bindings.GLProgram;
+import bindings.WebGLRenderContext;
+import bindings.GLTexture;
+import oglrenderer.GLLayer.RenderingElement;
+class TextureBinder implements RenderingElement {
+    var texture:GLTexture;
+    var image:Image;
+    public function new(image) {
+        this.image = image;
+    }
+     public function bind():Void {
+        gl.bindTexture(gl.TEXTURE_2D, texture);
+    }
+
+    public function unbind():Void {
+        gl.bindTexture(gl.TEXTURE_2D, null);
+    }
+
+    public function init(gl:WebGLRenderContext, program:GLProgram):Void {
+        this.gl = gl;
+        createTexture(gl, program);
+    }
+
+    var gl:WebGLRenderContext;
+
+    private function createTexture(gl:WebGLRenderContext, program):Void {
+//        var image = Assets.getImage("Assets/Blackberry.png");
+//        var image = ;
+        var imageUniform = gl.getUniformLocation(program, UniformAliases.IMG_0);
+        gl.uniform1i(imageUniform, 0);
+        texture = gl.createTexture();
+        gl.bindTexture(gl.TEXTURE_2D, texture);
+        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+        gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, image.buffer.width, image.buffer.height, 0, gl.RGBA, gl.UNSIGNED_BYTE, image.data);
+        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR_MIPMAP_LINEAR);
+        gl.generateMipmap(gl.TEXTURE_2D);
+        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+        gl.bindTexture(gl.TEXTURE_2D, null);
+    }
+}
