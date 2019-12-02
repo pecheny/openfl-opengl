7e0d367f6867faacb0137de384bd9142c528a907
artem pecheny <pecheny@gmail.com>
2019-12-03 00:11:39 +0300

    Quad indices initialization util.

--- src/data/IndexCollection.hx
+++ src/data/IndexCollection.hx
@@ -21,4 +21,18 @@ abstract IndexCollection(Bytes) from Bytes to Bytes {
     inline function get_length():Int {
         return Std.int(this.length / ELEMENT_SIZE);
     }
+
+    public static function forQuads(count) {
+        var ic = new IndexCollection(count * 6);
+        for (i in 0...count) {
+            var j = i*6;
+            ic[j] =  i * 4;
+            ic[j + 1] =  i * 4 + 1;
+            ic[j + 2] =  i * 4 + 2;
+            ic[j + 3] =  i * 4 ;
+            ic[j + 4] =  i * 4 + 3;
+            ic[j + 5] =  i * 4 + 2;
+        }
+        return ic;
+    }
 }
