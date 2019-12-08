325bc8dee66a1e1e874890388da3bc3ba4db3dbf
artem pecheny <pecheny@gmail.com>
2019-12-09 01:23:40 +0300

    Make AttributeAliases inline.

--- src/data/AttribAliases.hx
+++ src/data/AttribAliases.hx
@@ -2,13 +2,13 @@ package data;
 @:keep
 @:expose
 class AttribAliases {
-    public static var NAME_POSITION = "position";
-    public static var NAME_WEIGHT = "weight";
-    public static var NAME_MATRIX = "matrix";
-    public static var NAME_COLOR_IN = "colorIn";
-    public static var NAME_COLOR_OUT = "colorOut";
-    public static var NAME_SCREENSPACE_T = "t";
-    public static var NAME_SPEED = "speed";
-    public static var NAME_SIZE = "size";
-    public static var NAME_UV_0 = "uv0";
+    public static inline var NAME_POSITION = "position";
+    public static inline var NAME_WEIGHT = "weight";
+    public static inline var NAME_MATRIX = "matrix";
+    public static inline var NAME_COLOR_IN = "colorIn";
+    public static inline var NAME_COLOR_OUT = "colorOut";
+    public static inline var NAME_SCREENSPACE_T = "t";
+    public static inline var NAME_SPEED = "speed";
+    public static inline var NAME_SIZE = "size";
+    public static inline var NAME_UV_0 = "uv0";
 }
