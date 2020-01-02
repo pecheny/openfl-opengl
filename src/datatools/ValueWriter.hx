f077a12197cef3e84e923e5b9201126a6faaad6c
artem pecheny <pecheny@gmail.com>
2020-01-02 03:09:48 +0300

    Add start offset to ValueWriter.

--- src/datatools/ValueWriter.hx
+++ src/datatools/ValueWriter.hx
@@ -4,20 +4,21 @@ import data.AttributeDescr;
 import data.AttribSet;
 class ValueWriter<T> {
     var stride:Int;
-    var compOffset:Int;
+    var offset:Int;
     var type:DataType;
     var target:ByteDataWriter;
 
-    public function new(target:ByteDataWriter, attr:AttributeDescr, comp:Int, stride:Int) {
+    public function new(target:ByteDataWriter, attr:AttributeDescr, comp:Int, stride:Int, offset:Int = 0) {
         this.target = target;
-        compOffset = attr.offset + AttribSet.getGlSize(attr.type) * comp;
+        var compOffset = attr.offset + AttribSet.getGlSize(attr.type) * comp;
+        this.offset = compOffset + offset;
         this.stride = stride;
         this.type = attr.type;
     }
 
     // todo write macro typed set for dataWriter
     public inline function setValue(vertIdx:Int, value:T) {
-        target.setTyped(type, vertIdx * stride + compOffset, value);
+        target.setTyped(type, vertIdx * stride + offset, value);
     }
 }
 
