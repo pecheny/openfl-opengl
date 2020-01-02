a211c7626d8c09a6c0546fdc16c739a42b3e08b8
artem pecheny <pecheny@gmail.com>
2020-01-02 03:10:47 +0300

    Mute StaticMeshProvider.

--- src/mesh/StaticMeshProvider.hx
+++ src/mesh/StaticMeshProvider.hx
@@ -1,48 +1,47 @@
-package mesh;
-import data.IndexCollection;
-import mesh.providers.AttrProviders.SolidColorProvider;
-import mesh.providers.AttrProviders.TriPosProvider;
-import data.AttribAliases;
-import data.AttribSet;
-import gltools.ByteDataWriter;
-import gltools.sets.ColorSet;
-import gltools.VertDataProvider;
-import gltools.VertexBuilder;
-import haxe.io.Bytes;
-import haxe.io.UInt16Array;
-class StaticMeshProvider<T:AttribSet> implements VertDataProvider<T> {
-    var data:ByteDataWriter;
-    var inds:ByteDataWriter;
-
-    public function new() {
-        var builder = new VertexBuilder(ColorSet.instance);
-        builder.setTarget(Bytes.alloc(3 * ColorSet.instance.stride));
-        var pos = new TriPosProvider(1, -1);
-        var color = new SolidColorProvider(128, 10, 100);
-        builder.regSetter(AttribAliases.NAME_POSITION, pos.getPos);
-        builder.regSetter(AttribAliases.NAME_COLOR_IN, color.getCC);
-        builder.fetchFertices(3);
-        data = builder.getData();
-        inds = new IndexCollection(3);
-        for (i in 0...3)
-            inds[i] = i;
-    }
-
-    public function getVerts():Bytes {
-        return data;
-    }
-
-    public function getInds() {
-        return inds;
-    }
-
-    public function getVertsCount():Int {
-        return 3;
-    }
-
-    public function getIndsCount():Int {
-        return 3;
-    }
-
-
-}
+//package mesh;
+//import datatools.ByteDataWriter;
+//import data.AttribAliases;
+//import data.AttribSet;
+//import data.IndexCollection;
+//import gltools.sets.ColorSet;
+//import gltools.VertIndDataProvider;
+//import gltools.VertexBuilder;
+//import haxe.io.Bytes;
+//import mesh.providers.AttrProviders.SolidColorProvider;
+//import mesh.providers.AttrProviders.TriPosProvider;
+//class StaticMeshProvider<T:AttribSet> implements VertIndDataProvider<T> {
+//    var data:ByteDataWriter;
+//    var inds:ByteDataWriter;
+//
+//    public function new() {
+//        var builder = new VertexBuilder(ColorSet.instance);
+//        builder.setTarget(Bytes.alloc(3 * ColorSet.instance.stride));
+//        var pos = new TriPosProvider(1, -1);
+//        var color = new SolidColorProvider(128, 10, 100);
+//        builder.regSetter(AttribAliases.NAME_POSITION, pos.getPos);
+//        builder.regSetter(AttribAliases.NAME_COLOR_IN, color.getCC);
+//        builder.fetchFertices(3);
+//        data = builder.getData();
+//        inds = new IndexCollection(3);
+//        for (i in 0...3)
+//            inds[i] = i;
+//    }
+//
+//    public function getVerts():Bytes {
+//        return data;
+//    }
+//
+//    public function getInds() {
+//        return inds;
+//    }
+//
+//    public function getVertsCount():Int {
+//        return 3;
+//    }
+//
+//    public function getIndsCount():Int {
+//        return 3;
+//    }
+//
+//
+//}
