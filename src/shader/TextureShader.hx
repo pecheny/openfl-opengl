292b366d9be48f3badf8c2d6fee6cc002b38da8a
artem pecheny <pecheny@gmail.com>
2020-01-13 00:17:38 +0300

    Extract vertex and fragment shaders.

--- src/shader/TextureShader.hx
+++ src/shader/TextureShader.hx
@@ -1,13 +1,11 @@
 package shader;
-import Aliases.VA;
 import Aliases.UA;
-import lime.graphics.opengl.GLProgram;
+import Aliases.VA;
 import data.AttribAliases;
 import data.VaryingAliases;
+import lime.graphics.opengl.GLProgram;
 class TextureShader {
-
-    public static function createDummyShader(gl) {
-        var vs = '
+    public static inline var vs = '
                  attribute vec2 ${AttribAliases.NAME_POSITION};
                  attribute vec2 ${AttribAliases.NAME_UV_0};
                  varying vec2 ${VaryingAliases.UV_0};
@@ -17,16 +15,17 @@ class TextureShader {
                    ${VaryingAliases.UV_0} = ${AttribAliases.NAME_UV_0};
                  }';
 
-        var fs =
-            #if (!desktop || rpi)
-                "precision mediump float;" +
-            #end
-                'varying vec2 ${VA.UV_0};
+    public static inline var fs =
+        #if (!desktop || rpi)
+    "precision mediump float;" +
+        #end
+    'varying vec2 ${VA.UV_0};
                  uniform sampler2D ${UA.IMG_0};
                  void main(){
 				    gl_FragColor = texture2D (${UA.IMG_0}, ${VA.UV_0});
                  }';
 
+    public static function createDummyShader(gl) {
         return GLProgram.fromSources(gl, vs, fs);
     }
 }
