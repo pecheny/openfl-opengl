074037da95a6c43981dc3983d23dd57355d78de5
artem pecheny <pecheny@gmail.com>
2020-07-09 15:49:10 +0300

    Remove unused method.

--- src/MeshUtils.hx
+++ src/MeshUtils.hx
@@ -1,19 +1,9 @@
 package ;
 import data.AttribSet;
-import data.AttribSources;
-import mesh.I2DCompoundDP.I2DCompoundTypedDP;
 import data.AttributeDescr;
 import data.DataType;
 import data.VertexAttribProvider;
 class MeshUtils {
-    public static function convertToI2DC<T:AttribSet>(set:T, sources:AttribSources<T>, vertCount, indCount):I2DCompoundTypedDP<T> {
-        var i2d = new I2DCompoundTypedDP(set);
-        for (name in sources.keys()) {
-            i2d.addDataSource(name, sources.get(name));
-        }
-        i2d.fetchVertices(vertCount, indCount);
-        return i2d;
-    }
 
     public static function writeInt8Attribute<T:AttribSet>(attrs:AttribSet, bytes:haxe.io.Bytes, attName:String, firstVert:Int, vertCount:Int, provider:VertexAttribProvider) {
         var descr:AttributeDescr = attrs.getDescr(attName);
