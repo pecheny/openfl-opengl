6d4b6c8c82ae511b0cfa63e44c92c46d33e3ceda
artem pecheny <pecheny@gmail.com>
2020-09-22 16:42:08 +0300

    AspectRatio.

---
+++ src/transform/AspectRatioProvider.hx
@@ -0,0 +1,6 @@
+package transform;
+import haxe.ds.ReadOnlyArray;
+interface AspectRatioProvider {
+    public function getFactorsRef():ReadOnlyArray<Float>;
+}
+
--- src/transform/AspectTransform.hx
+++ src/transform/AspectTransform.hx
@@ -1,24 +1,22 @@
 package transform;
-import al.al2d.Boundbox;
-import haxe.ds.ReadOnlyArray;
-import openfl.events.Event;
-import datatools.VertValueProvider;
 import al.al2d.Axis2D;
+import al.al2d.Boundbox;
 import al.core.AxisApplier;
+import datatools.VertValueProvider;
 
 //todo make own boundbox, exclude al dependency
 class AspectTransform implements VertValueProvider {
     var prv:VertValueProvider;
     var pos:Array<Float> = [0, 0];
     var size:Array<Float> = [1, 1];
-    var aspects:StageAspectKeeper;
+    var aFactors:AspectRatio;
     var bounds:Boundbox = new Boundbox();
     var appliers:Map<Axis2D, AxisApplier>;
     var bbAspect:Float = 1;
     var localScale = 1.;
 
-    public function new(aspects:StageAspectKeeper, prv) {
-        this.aspects = aspects;
+    public function new(aspects:AspectRatioProvider, prv) {
+        aFactors = aspects.getFactorsRef();
         this.prv = prv;
     }
 
@@ -32,12 +30,10 @@ class AspectTransform implements VertValueProvider {
         var free = size[c] - bounds.size[a] * localScale;
         var lp = (input - bounds.pos[a]) * localScale + free / 2;
         return
-            (pos[c] + lp) / aspects.getFactor(c) - 1;
-//             pos[c] * aspects.getFactor(c) + prv.getValue(v,c) *  aspects.getFactor(c) * 0.25;
+            (pos[c] + lp) / aFactors[c] - 1;
     }
 
     public function getValue(v:Int, c:Int):Float {
-//        var ls = size[c];
         return transformValue(c, prv.getValue(v, c));
     }
 
@@ -86,46 +82,46 @@ class AspectTransformAxisApplier implements AxisApplier {
 }
 
 
-interface AspectFactorsProvider {
-    public function getFactorsRef():ReadOnlyArray<Float>;
-}
-class StageAspectKeeper implements AspectFactorsProvider {
-    var base:Float;
-
-    var factors:Array<Float> = [1, 1];
-
-
-    public function new(base:Float = 1) {
-        this.base = base;
-        openfl.Lib.current.stage.addEventListener(Event.RESIZE, onResize);
-        onResize(null);
-    }
-
-    function onResize(e) {
-        var stage = openfl.Lib.current.stage;
-        var width = stage.stageWidth;
-        var height = stage.stageHeight;
-        if (width > height) {
-            factors[0] = (base * width / height);
-            factors[1] = base;
-        } else {
-            factors[0] = base;
-            factors[1] = (base * height / width);
-        }
-//        trace(factors);
-    }
-
-    public inline function getFactor(cmp:Int):Float {
-        return factors[cmp];
-    }
-
-    public function getFactorsRef():ReadOnlyArray<Float> {
-        return factors;
-    }
-
-    public function getTransform(attribute:String, a:VertValueProvider):AspectTransform {
-        var tr = new AspectTransform(this, a);
-        return tr;
-    }
-
-}
+//interface AspectFactorsProvider {
+//    public function getFactorsRef():ReadOnlyArray<Float>;
+//}
+//class StageAspectKeeper implements AspectFactorsProvider {
+//    var base:Float;
+//
+//    var factors:Array<Float> = [1, 1];
+//
+//
+//    public function new(base:Float = 1) {
+//        this.base = base;
+//        openfl.Lib.current.stage.addEventListener(Event.RESIZE, onResize);
+//        onResize(null);
+//    }
+//
+//    function onResize(e) {
+//        var stage = openfl.Lib.current.stage;
+//        var width = stage.stageWidth;
+//        var height = stage.stageHeight;
+//        if (width > height) {
+//            factors[0] = (base * width / height);
+//            factors[1] = base;
+//        } else {
+//            factors[0] = base;
+//            factors[1] = (base * height / width);
+//        }
+////        trace(factors);
+//    }
+//
+//    public inline function getFactor(cmp:Int):Float {
+//        return factors[cmp];
+//    }
+//
+//    public function getFactorsRef():ReadOnlyArray<Float> {
+//        return factors;
+//    }
+//
+//    public function getTransform(attribute:String, a:VertValueProvider):AspectTransform {
+//        var tr = new AspectTransform(this, a);
+//        return tr;
+//    }
+//
+//}
